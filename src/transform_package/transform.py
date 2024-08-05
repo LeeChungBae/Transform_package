298de2cf349f5511debe29146646300e8b270301
NishNovae <raecrowned@gmail.com>
2024-08-05 18:04:54 +0900

    something wrong

--- src/transform_package/transform.py
+++ src/transform_package/transform.py
@@ -23,7 +23,7 @@ def str_to_num(df, path):
                 'audiInten', 'audiChange',  # 관객수 변화 절대값 & 비율
      ]
     for col in num_cols:
-        df[col] = pd.to_numeric(df[col])i
+        df[col] = pd.to_numeric(df[col])
 
     df.to_parquet(path, partition_cols=['load_dt'])
     return df
