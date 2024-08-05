7785eae968710a85d8142ed7ba685c8ea9a7261a
NishNovae <raecrowned@gmail.com>
2024-08-05 18:39:44 +0900

    pls

--- src/transform_package/transform.py
+++ src/transform_package/transform.py
@@ -7,22 +7,20 @@ import pandas as pd
 # 행 추출
 def col_drop(load_dt, path):
     df = pd.read_parquet(f"{path}/load_dt={load_dt}")
-    print('flag1')
-    print(type(df))
 
     # drop columns -> implement
     cols = ['rnum','rankOldAndNew','movieCd'] 
-    df = df.drop(axis=1,columns=cols)   
-
-    print('flag2')
+    df = df.drop(columns = cols, axis = 1)
+    
     print(type(df))
+
+    df = pd.DataFrame(df)
+    print(type(df))
+ 
     return df
 
 # 문자열 > 숫자
 def str_to_num(df, path):
-    # columns to sum
-    print('flag3')
-    print(type(df))
     num_cols = ['rank',                     # 순위
                 'salesAmt', 'salesAcc',     # 일일 & 누적 매출액
                 'salesShare',               # 전체 영화 매출액 중 파이 비율
