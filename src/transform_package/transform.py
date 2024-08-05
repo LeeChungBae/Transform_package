23fc722edd8a0358f5f345a066d3a0be4628c0cf
NishNovae <raecrowned@gmail.com>
2024-08-05 18:28:06 +0900

    debug flags

--- src/transform_package/transform.py
+++ src/transform_package/transform.py
@@ -7,14 +7,17 @@ import pandas as pd
 # 행 추출
 def col_drop(load_dt, path):
     df = pd.read_parquet(f"{path}/load_dt={load_dt}")
+    print(type(df))
     # drop columns -> implement
     cols = ['rnum','rankOldAndNew','movieCd']
-    df=df.drop(axis=1,columns=cols)   
+    print(type(df))
+    df = df.drop(axis=1,columns=cols)   
     return df
 
 # 문자열 > 숫자
 def str_to_num(df, path):
     # columns to sum
+    print(type(df))
     num_cols = ['rank',                     # 순위
                 'salesAmt', 'salesAcc',     # 일일 & 누적 매출액
                 'salesShare',               # 전체 영화 매출액 중 파이 비율
@@ -23,7 +26,6 @@ def str_to_num(df, path):
                 'audiInten', 'audiChange',  # 관객수 변화 절대값 & 비율
      ]
     for col in num_cols:
-        print(type(df))
         print(df[col], type(df[col]))
         df[col] = pd.to_numeric(df[col])
 
